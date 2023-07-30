67ae8b5f913a88cd5cc723023a6bddb841c06dc5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-30 21:56:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+# Read in data from the Google Sheets
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-st.write('Hello world!')
+df = load_data(st.secrets["public_gsheets_url"])
+st.data_editor(df)
