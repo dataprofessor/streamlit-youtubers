cf5e82e4cb024b851d49a13202ffde707be3e484
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-30 22:44:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,18 @@ def load_data(sheets_url):
     return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
+
+column_configuration = {
+    "name": st.column_config.TextColumn(
+        "Name", help="Name of YouTube channels", max_chars=100
+    ),
+    "youtube_id": st.column_config.TextColumn(
+        "YouTube Channel ID", help="ID of YouTube channels", max_chars=100
+    ),
+    "avatar": st.column_config.ImageColumn("Avatar", help="YouTube channel's avatar"),
+    "streamlit_playlist": st.column_config.TextColumn(
+        "Streamlit Playlist", help="Playlist of Streamlit tutorial videos", max_chars=100
+    )
+}
+
 st.data_editor(df)
